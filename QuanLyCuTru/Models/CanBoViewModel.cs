5021059606cf4e42b4ea4fa781f27547062d21cd
Jon <minhquanma@gmail.com>
2018-04-14 17:11:50 +0700

    Modified QuanLyCuTru stuffs

---
+++ QuanLyCuTru/Models/CanBoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyCuTru.Models
+{
+    public class CanBoViewModel
+    {
+        public string TenChucVu { get; set; }
+
+    }
+}
