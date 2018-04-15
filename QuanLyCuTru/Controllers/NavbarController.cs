8b0c4e161ade1259056c2657a04584d5175e17f3
Jon <minhquanma@gmail.com>
2018-04-15 12:05:02 +0700

    Added NavBar user's avatar

---
+++ QuanLyCuTru/Controllers/NavbarController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using QuanLyCuTru.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyCuTru.Controllers
+{
+    public class NavbarController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public NavbarController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string uid = User.Identity.GetUserId();
+                NguoiDung nguoiDung = db.NguoiDungs.SingleOrDefault(u => u.IdentityId.Contains(uid));
+
+                return PartialView("_NavBar", nguoiDung);
+            }
+            return PartialView("_NavBar");
+        }
+    }
+}
