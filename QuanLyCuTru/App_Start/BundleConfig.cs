8dd7704d6e5f3b6c27415931bc1fe11b9a041361
Jon <minhquanma@gmail.com>
2018-07-17 18:32:15 +0700

    Refactored Bundles, fixed "Trở về" for QuanLyCongDan using JS, Changed SQL connectionString

--- QuanLyCuTru/App_Start/BundleConfig.cs
+++ QuanLyCuTru/App_Start/BundleConfig.cs
@@ -8,44 +8,41 @@ namespace QuanLyCuTru
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Jquery
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+            // Jquery val
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-
+            // Jquery ui
             bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
                         "~/Scripts/jquery-ui*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                        "~/Scripts/popper*"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            // Bootstrap JS
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.min.js",
                       "~/Scripts/bootbox.min.js",
                       "~/Scripts/respond.js"));
 
+            // Bootstrap CSS
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.pulse.min.css",
                       "~/Content/site.css",
                       "~/Content/PagedList.css"));
 
+            // Jquery ui CSS
+            bundles.Add(new StyleBundle("~/Content/jquery-ui/base/css").Include(
+              "~/Content/jquery-ui/base/theme.css",
+              "~/Content/jquery-ui/base/jquery-ui.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-              "~/Content/themes/base/core.css",
-              "~/Content/themes/base/resizeable.css",
-              "~/Content/themes/base/selectable.css",
-              "~/Content/themes/base/accordion.css",
-              "~/Content/themes/base/autocomplete.css",
-              "~/Content/themes/base/button.css",
-              "~/Content/themes/base/dialog.css",
-              "~/Content/themes/base/slider.css",
-              "~/Content/themes/base/tabs.css",
-              "~/Content/themes/base/datepicker.css",
-              "~/Content/themes/base/progressbar.css",
-              "~/Content/themes/base/theme.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
