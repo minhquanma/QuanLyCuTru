8734f6a072e517eef19c71e6c8e5cdef6349afa6
Nguyen Thanh Luong <ntluong823@gmail.com>
2018-08-20 13:30:05 +0700

    Xóa form1 thừa

--- QuanLyCuTru_WinForm/Program.cs
+++ QuanLyCuTru_WinForm/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyCuTru_WinForm
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new BackgroundImage());
-            Application.Run(new FormDangNhap());
+            Application.Run(new FormCanBoQuanLy());
         }
     }
 }
